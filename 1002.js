1ec670d9166c870bb1f8c884b16cdb4b2cb9f294
Gabriel Ferreira Miquelin <gabrielferreiramiquelin@gmail.com>
2023-05-05 08:28:40 -0300

    update 1003.js

--- 1002.js
+++ 1002.js
@@ -8,17 +8,17 @@
 // Saída
 // Apresentar a mensagem "A=" seguido pelo valor da variável area, conforme exemplo abaixo, com 4 casas após o ponto decimal. Utilize variáveis de dupla precisão (double). Como todos os problemas, não esqueça de imprimir o fim de linha após o resultado, caso contrário, você receberá "Presentation Error"
 
-let input = require("fs").readFileSync("stdin","utf8");
-let valor = input.split('\n');
+// let input = require("fs").readFileSync("stdin","utf8");
+// let valor = input.split('\n');
 
-let n = 3.14159;
-let raio = valor.shift();
+// let n = 3.14159;
+// let raio = valor.shift();
 
-// let area = (raio * raio) * pi 
- SEGUNDA FORMA
-let area = n * Math.pow(raio,2);//raio ao quadrado
+// // let area = (raio * raio) * pi 
+//  SEGUNDA FORMA
+// let area = n * Math.pow(raio,2);//raio ao quadrado
 
-console.log(`A= ${area.toFixed(4)}`);
+// console.log(`A= ${area.toFixed(4)}`);
 
 
 //OBS: O codigo vai estar com erro posi você precisa comentar um dos dois!
--- readme.md
+++ readme.md
@@ -1,9 +1,9 @@
 <h2>Desafios Beecrowd</h2>
-
-<h3>Linguagem Utilizada</3>
-
-<li>JavaScript</li>
-
+<hr>
+<h3>Linguagem Utilizada:</3>
+JavaScript
+<br>
+<hr>
 <h3>Tentando Resolver os 336 desafio do <a src="https://www.beecrowd.com.br/judge/pt/problems/index/1">Beecrowd</a> iniciante</h3>
 
 <h4>Registrando tudo aqui no GitHub e mostrando minha evolução</h4>
