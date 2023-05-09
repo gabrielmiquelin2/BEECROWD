81c517ef4209e5074fd5d214861daad988b502a2
Gabriel Ferreira Miquelin <gabrielferreiramiquelin@gmail.com>
2023-05-09 09:50:12 -0300

    update exercicios beecrowd

--- 1010.js
+++ 1010.js
@@ -7,6 +7,23 @@
 // Saída
 // A saída deverá ser uma mensagem conforme o exemplo fornecido abaixo, lembrando de deixar um espaço após os dois pontos e um espaço após o "R$". O valor deverá ser apresentado com 2 casas após o ponto.
 
-// let input = require('fs').readFileSync('stdin', 'utf8');
-// let lines = input.split('\n');//shift remove o primeiro elemento de um array e depois o retorna
+let input = require('fs').readFileSync('stdin', 'utf8');
+let lines = input.split('\n');
+
+// Lê os valores de entrada
+let valores1 = lines[0].split(" ");
+let codigoPeca1 = parseInt(valores1[0]);
+let numPecas1 = parseInt(valores1[1]);
+let valorPeca1 = parseFloat(valores1[2]);
+
+let valores2 = lines[1].split(" ");
+let codigoPeca2 = parseInt(valores2[0]);
+let numPecas2 = parseInt(valores2[1]);
+let valorPeca2 = parseFloat(valores2[2]);
+
+// Calcula o valor total
+let valorTotal = (numPecas1 * valorPeca1) + (numPecas2 * valorPeca2);
+
+// Exibe a mensagem de saída
+console.log(`VALOR A PAGAR: R$ ${valorTotal.toFixed(2)}`);
 
